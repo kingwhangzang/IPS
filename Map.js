ead2d327f481129695d9fcc12c98457beecb6913
sera <113303445+seraparrk@users.noreply.github.com>
2023-05-22 20:40:29 +0900

    Add files via upload

--- Map.js
+++ Map.js
@@ -1,42 +1,72 @@
-//eslint-disable-next-line
-/* global kakao */
 import React, { useEffect, useState } from 'react';
+import { NavLink } from 'react-router-dom';
 import ReviewAndInfo from './ReviewAndInfo'; 
 
-const {kakao} = window;
+const { kakao } = window;
 
-function Map(){
+function Map() {
   const [selectedStore, setSelectedStore] = useState(null);
 
-  useEffect(()=>{
+  const handleLogout = () => {
+    // Logout logic implementation
+  };
+
+  useEffect(() => {
     const container = document.getElementById('map');
     const options = {
       center: new kakao.maps.LatLng(37.365264512305174, 127.10676860117488),
       level: 3
     };
-    //eslint-disable-next-line
-    const map = new kakao.maps.Map(container, options); 
+    const map = new kakao.maps.Map(container, options);
 
-    const markerPosition = new kakao.maps.LatLng(37.56000302825312, 126.97540593203321); 
-        const marker = new kakao.maps.Marker({ 
-          position: markerPosition
-        }); 
+    const markerPosition = new kakao.maps.LatLng(37.56000302825312, 126.97540593203321);
+    const marker = new kakao.maps.Marker({
+      position: markerPosition
+    });
 
-        kakao.maps.event.addListener(marker, 'click', function() {
-          setSelectedStore({
-            name: '가맹점 이름',  // 가맹점의 실제 이름으로 대체하세요.
-            review: '리뷰 내용', // 실제 리뷰 내용으로 대체하세요.
-            info: '가맹점 정보'   // 실제 가맹점 정보로 대체하세요.
-          });
-        });
-        marker.setMap(map); 
-    }, [])
+    kakao.maps.event.addListener(marker, 'click', function() {
+      setSelectedStore({
+        name: '가맹점 이름',
+        review: '리뷰 내용',
+        info: '가맹점 정보'
+      });
+    });
+    marker.setMap(map);
+  }, []);
 
+  return (
+    <div>
+      <nav className="dashboard-nav">
+        <ul>
+          <li><img src={process.env.PUBLIC_URL + '/friend.png'} alt="friend" /></li>
+          <li className='sook'>쑥쑥카드</li>
+          <li>
+            <NavLink to="/dashboard" activeClassName="active">홈</NavLink>
+          </li>
+          <li>
+            <NavLink to="/map" activeClassName="active">가맹점 지도 조회</NavLink>
+          </li>
+          <li>
+            <NavLink to="/health" activeClassName="active">나의 건강</NavLink>
+          </li>
+          <li>
+            <NavLink to="/mypage" activeClassName="active">마이페이지</NavLink>
+          </li>
+          <li>
+            <button onClick={handleLogout}>Logout</button>
+          </li>
+        </ul>
+      </nav>
 
-    return (
-      <div>
-      <div id="map" style={{width:"2000px", height:"800px"}}></div>
-      {selectedStore && <ReviewAndInfo name={selectedStore.name} review={selectedStore.review} info={selectedStore.info} />}
+      <div id="map" style={{ width: "1000px", height: "400px" }}></div>
+
+      {selectedStore && (
+        <ReviewAndInfo
+          name={selectedStore.name}
+          review={selectedStore.review}
+          info={selectedStore.info}
+        />
+      )}
     </div>
   );
 }
