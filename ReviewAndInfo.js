3dce5a78611d108137107ff4ad716e46b3344e33
sera <113303445+seraparrk@users.noreply.github.com>
2023-05-24 13:58:47 +0900

    Add files via upload

--- ReviewAndInfo.js
+++ ReviewAndInfo.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import StarRating from './StarRating';
+import './Map.css';
 
-function ReviewAndInfo({ title, review, info1, info2, info3 }) {
+function ReviewAndInfo({ title, address, num, info1, review, info3 }) {
   return (
-    <div>
+    <div className='storeinfo'>
       <h2>가게 이름: {title}</h2>
-      <p>리뷰: {review}</p>
+      <div className='box'>
+      <p>주소: {address}</p>
+      <p>연락처: {num}</p>
       <p>메뉴정보: {info1}</p>
-      <p>연락처: {info2}</p>
+      <p>리뷰정보: {review}</p>
       <p>평점: <StarRating rating={info3} /></p>
+      </div>
     </div>
   );
 }
